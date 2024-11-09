91b6e8033f768fe438b63b074ee4af6d1a9906be
Leon Siqueira <leon.siqueir4@gmail.com>
2024-11-08 22:59:25 -0300

    fix: add not_found method on the parent to be used in any controller feat: params filter on Controllers

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -6,11 +6,6 @@ use App\Core\Controller;
 
 class HomeController extends Controller
 {
-  public function not_found($first = false)
-  {
-    $this->view('not_found');
-  }
-
   public function index()
   {
     $this->view('home');
--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -4,15 +4,27 @@ namespace App\Core;
 
 class Controller
 {
-  public function view(string $view, array $data = [])
+  protected function view(string $view, array $data = [])
   {
     require substr($_SERVER['DOCUMENT_ROOT'], 0, -7) . '/src/Views/' . $view . '.php';
   }
 
-  public function redirect($uri, $statusCode = 302)
+  protected function redirect($uri, $statusCode = 302)
   {
       http_response_code($statusCode);
       header("Location: $uri");
       exit();
+  }
+
+  protected function not_found($first = false)
+  {
+    $this->view('not_found');
+  }
+
+  protected function permit_params(array $params, array $allowed)
+  {
+    return array_filter($params, function($key) use ($allowed) {
+      return in_array($key, $allowed);
+    }, ARRAY_FILTER_USE_KEY);
   }
 }
