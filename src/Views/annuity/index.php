78ab2797e677ab26779b845603b8bbdf403b31cd
Leon Siqueira <leon.siqueir4@gmail.com>
2024-11-10 21:23:10 -0300

    feat: enhance views - layout and annuity/index

--- src/Views/annuity/index.php
+++ src/Views/annuity/index.php
@@ -3,13 +3,44 @@
   $title = 'Annuities';
   require PathsHelper::layout();
 ?>
-<h1>Annuities</h1>
-<ul>
-  <?php foreach ($data['annuities'] as $annuity): ?>
-    <li>
-      <a href="/annuity/show/<?= $annuity['year'] ?>">
-        Annuity - Year <?= $annuity['year'] ?>
+<div class="container-fluid p-4">
+  <div class="row justify-content-between">
+    <div class="col-2">
+      <h2>Annuities</h2>
+    </div>
+    <div class="col-4 d-flex justify-content-end align-items-center">
+      <a href="/annuity/new" class="btn btn-primary">
+        <i class="bi bi-plus"></i>
+        <span>
+          Add
+        </span>
       </a>
-    </li>
-  <?php endforeach; ?>
-</ul>
+    </div>
+  </div>
+  <div class="row justify-content-center pt-4">
+    <div class="col-md-8 table-responsive">
+      <table class="table table-dark table-striped align-middle">
+        <thead>
+          <tr>
+            <th scope="col">Year</th>
+            <th scope="col">Value</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($data['annuities'] as $annuity): ?>
+            <tr>
+              <td><?= $annuity['year'] ?></td>
+              <td><?= $annuity['value'] ?></td>
+              <td class="d-flex justify-content-end" >
+                <a href="/annuity/show/<?= $annuity['year'] ?>" class="btn btn-sm btn-secondary">
+                  <i class="bi bi-eye"></i>
+                  Show
+                </a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+      </table>
+    </div>
+  </div>
+</div>
--- src/Views/layout.php
+++ src/Views/layout.php
@@ -2,22 +2,28 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?? 'default title' ?></title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   </head>
   <body>
-  <ul>
-    <li>
-      <a href="/annuity/new">New Annuity</a>
-    </li>
-    <li>
-      <a href="/annuity/index">List Annuities</a>
-    </li>
-    <li>
-      <a href="/member/new">New Member</a>
-    </li>
-    <li>
-      <a href="/member/index">List Members</a>
-    </li>
-  </ul>
+  <nav class="navbar navbar-expand navbar-light bg-light p-2">
+    <div class="container-fluid flex-wrap">
+      <a class="navbar-brand fs-2 fw-bold" href="/">Home</a>
+      <ul class="navbar-nav my-1 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/annuity/index">Annuities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/member/index">Members</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/payments/index">Payments</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <script src="/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
