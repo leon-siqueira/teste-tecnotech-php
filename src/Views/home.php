a6d84738f2e1ff733336ad6c68e9ee9ebbba1128
Leon Siqueira <leon.siqueir4@gmail.com>
2024-11-11 21:35:04 -0300

    feat: enhance home and not found pages with improved layout and navigation

--- src/Views/home.php
+++ src/Views/home.php
@@ -2,4 +2,15 @@
   $title = 'Home';
   require __DIR__.'/layout.php';
 ?>
-Welcome to the home page
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1 class="display-4">Devs do RN</h1>
+      <p class="lead">Check the information about members, annuities and checkouts in the following links.</p>
+      <hr class="my-4">
+      <a class="btn btn-primary btn-lg m-3" href="annuity" role="button">Annuities</a>
+      <a class="btn btn-primary btn-lg m-3" href="member" role="button">Members</a>
+      <a class="btn btn-primary btn-lg m-3" href="checkout" role="button">Checkouts</a>
+    </div>
+  </div>
+</div>
--- src/Views/not_found.php
+++ src/Views/not_found.php
@@ -3,4 +3,22 @@
   $title = 'Page not found';
   require __DIR__.'/layout.php';
 ?>
-<h1>Page not found</h1>
+<div class="container text-center mt-5">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="">
+        <h1>Oops!</h1>
+        <h2>404 Not Found</h2>
+        <div class="my-4">
+          Sorry, an error has occurred, the requested page was not found!
+        </div>
+        <div class="error-actions">
+          <a href="/" class="btn btn-primary btn-lg">
+            <span class="glyphicon glyphicon-home"></span>
+            Take Me Home
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
